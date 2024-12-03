e4e6efda1508b93d41d1caa56416bde33d73c008
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-12-03 15:48:57 +0000

    Refactored + added day 2

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -3,21 +3,23 @@ defmodule Day1 do
   Documentation for `Day1`.
   """
   
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day1.hello()
-      :world
-
-  """
-  def hello do
+  def part_one do
     {ls1, ls2} = read_input_data()
 
     [Enum.sort(ls1), Enum.sort(ls2)]
     |> Enum.zip()
     |> Enum.map(fn {first, second} -> abs(first - second) end)
+    |> Enum.sum()
+  end
+
+  def part_two do
+    {ls1, ls2} = read_input_data()
+
+    map = compute_frequency(ls2)
+
+    ls1
+    |> Enum.filter(&Map.has_key?(map, &1)) # Filtering only existing one, the others have null contribution
+    |> Enum.map(fn x -> x * Map.get(map, x) end)
     |> Enum.sum()
   end
 
@@ -32,5 +34,17 @@ defmodule Day1 do
     s
     |> String.replace("\n", "")
     |> String.to_integer()
+  end
+
+  defp compute_frequency(ls, acc \\ Map.new())
+
+  defp compute_frequency([], map), do: map
+
+  defp compute_frequency([first | rest], map) do
+    if Map.has_key?(map, first) do
+      compute_frequency(rest, Map.update!(map, first, & &1 + 1))
+    else
+      compute_frequency(rest, Map.put_new(map, first, 1))
+    end
   end
 end
---
+++ day2/lib/day2.ex
@@ -0,0 +1,8 @@
+defmodule Day2 do
+  @moduledoc """
+  Documentation for `Day2`.
+  """
+
+  def part_one do
+  end
+end
